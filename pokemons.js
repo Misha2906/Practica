7c967484bdc3c478839b3a2edd47fd085372e010
Misha2906 <130909885+Misha2906@users.noreply.github.com>
2024-02-01 10:56:44 +0200

    Add files via upload

---
+++ pokemons.js
@@ -0,0 +1,194 @@
+export const pokemons = [
+    {
+        name: 'Pikachu',
+        type: 'electric',
+        hp: 274,
+        img: './assets/pick.png',
+        attacks: [
+            {
+                name: "thunder jolt",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "electro ball",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "volt tackle",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "thunder crack",
+                maxDamage: 160,
+                minDamage: 130,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: './assets/2pick.png',
+        name: 'Charmander',
+        type: 'fire',
+        hp: 282,
+        attacks: [
+            {
+                name: "ember",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "flamethrower",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "burning tail",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "fire spin",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQZJxSmnUCn3QYfikeKJjvRVGnbmvo1s9lEi8I3aQJr4xrDv8YqC5CTBp83VJy8u_TvX8&usqp=CAU',
+        name: 'Bulbasaur',
+        type: 'fire',
+        hp: 294,
+        attacks: [
+            {
+                name: "tackle",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "vine whip",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "razor leaf",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "solar beam",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://static.wikia.nocookie.net/pokemon-encyclopedia/images/3/39/007Squirtle.png/revision/latest/scale-to-width-down/250?cb=20171129150428',
+        name: 'Squirtle',
+        type: 'water',
+        hp: 292,
+        attacks: [
+            {
+                name: "bubble",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "water gun",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "shell attack",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "hydro pump",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png',
+        name: 'Pidgey',
+        type: 'air',
+        hp: 284,
+        attacks: [
+            {
+                name: "Air Slash",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Razor Wind",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Heat Wave",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Sky Attack",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://static.wikia.nocookie.net/vsbattles/images/b/b8/Poke_mew.png/revision/latest?cb=20170217060051',
+        name: 'Mew',
+        type: 'air',
+        hp: 404,
+        attacks: [
+            {
+                name: "Pound",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Ancient Power",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Aura Sphere",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Psychic",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    }
+];
